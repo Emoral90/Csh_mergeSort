26cc7c1bf390b7300bc3578373fef7d9cc84f546
gasolina90 <moralerick90@gmail.com>
2023-02-25 17:56:56 -0700

    modified file

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MergeSort
+{
+    // Attributes
+    private List<int> innerList = new List<int> {1,2,3};
+    
+
+    // Methods
+}
+
+class Program
+{
+    // Main entry point
+    static void Main()
+    {
+        // Constructor
+        MergeSort ms = new MergeSort();
+    }
+}
